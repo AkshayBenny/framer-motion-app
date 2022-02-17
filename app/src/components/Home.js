5d56ed219368bddfed6cb90a323f548075bff65a
AkshayBenny <akshaybennygh@gmail.com>
2022-02-17 16:42:27 +0530

    type, stiffness and transition added

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -4,10 +4,24 @@ import { motion } from 'framer-motion';
 
 const Home = () => {
   return (
-    <motion.div initial={{opacity:0}} animate={{opacity: 1}} className='home container'>
-      <h2>Welcome to Pizza Joint</h2>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className='home container'
+    >
+      <motion.h2
+          initial={{ opacity: 0 }}
+          transition={{ delay: 0.5 }}
+          animate={{  opacity: 1 }}
+      >
+        Welcome to Pizza Joint
+      </motion.h2>
       <Link to='/base'>
-        <motion.button animate={{ scale: 1.2 }}>
+        <motion.button
+          initial={{ opacity: 0 }}
+          transition={{ delay: 0.5 }}
+          animate={{  opacity: 1 }}
+        > 
           Create Your Pizza
         </motion.button>
       </Link>
