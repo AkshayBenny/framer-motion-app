047f2f619256e8031836dbed4beecfd384fc1188
AkshayBenny <akshaybennygh@gmail.com>
2022-02-17 19:37:36 +0530

    cleanup

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -2,26 +2,32 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
+const containerVariant = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+};
+
+const buttonVariant = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+  hover: {
+    scale: 1.2,
+    textShadow: '0px 0px 8px rgb(255, 255, 255)',
+    boxShadow: '0px 0px 8px rgb(255, 255, 255)',
+  },
+};
+
 const Home = () => {
   return (
     <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
+      variants={containerVariant}
+      initial='hidden'
+      animate='visible'
       className='home container'
     >
-      <motion.h2 initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-        Welcome to Pizza Joint
-      </motion.h2>
+      <h2>Welcome to Pizza Joint</h2>
       <Link to='/base'>
-        <motion.button
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          whileHover={{
-            scale: 1.2,
-            textShadow: '0px 0px 8px rgb(255, 255, 255)',
-            boxShadow: '0px 0px 8px rgb(255, 255, 255)',
-          }}
-        >
+        <motion.button variants={buttonVariant} whileHover='hover'>
           Create Your Pizza
         </motion.button>
       </Link>
